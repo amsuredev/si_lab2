99a97c4179d88ed6763ce910ce3b862f74e5047a
amsuredev <71019216+amsuredev@users.noreply.github.com>
2021-04-18 22:17:04 +0200

    Implement value heuristic

--- problem.py
+++ problem.py
@@ -23,8 +23,14 @@ class Problem:
         self.__dziedzina = dziedzina
 
         self.change_dziedzina_pers = change_dziedzina
+        self.__used_values = dict()
+        for val in self.d:
+            self.__used_values[val] = 0
 
-
+    def getSortedValsUpDown(self):
+        up_down_values = {k: v for k, v in
+                                sorted(self.__used_values.items(), key=lambda item: item[1], reverse=True)}
+        return up_down_values.keys()
 
     @property
     def solution(self):
@@ -53,6 +59,7 @@ class Problem:
                     self.__solution[x][y] = -1
         self.__solution[x][y] = -1
         return
+
 
     def forward_checking_statistic(self, point_ind, option_index):
         dziedzina_copy = deepcopy(self.__dziedzina)
@@ -99,6 +106,33 @@ class Problem:
         self.solution[x][y] = -1
         return
 
+    def get_most_used_value_first(self, x, y):
+        up_down = self.getSortedValsUpDown()
+        for val in up_down:
+            Problem.counter_backtracking += 1
+            if self.is_save(x, y, val):
+                self.solution[x][y] = val
+                used = True
+                self.__used_values[val] += 1
+                if y != self.__y - 1:
+                    #print(self.solution)
+                    if self.get_most_used_value_first(x, y+1):
+                        return True
+                    else:
+                        self.__used_values[val] -= 1
+                elif y == self.__y - 1 and x != self.__x - 1:
+                    if self.get_most_used_value_first(x + 1, 0):
+                        #print(self.solution)
+                        return True
+                    else:
+                        self.__used_values[val] -= 1
+                else:
+                    #print("success; ", "solution found: ", self.__solution)
+                    print("Counter backtracking: {count}".format(count=Problem.counter_backtracking))
+                    return True
+        self.solution[x][y] = -1
+        return
+
     def forward_checking_first_solution(self, point_ind, option_index):
         dziedzina_copy = deepcopy(self.__dziedzina)
         for val in self.__dziedzina[point_ind, option_index]:
@@ -130,13 +164,21 @@ class Problem:
 
 
 if __name__ == "__main__":
-    # graph = Graph(num_points=30, max_color_num=3)
-    # graph_problem = Problem(1, 30, graph.is_save, graph.max_color_num)
-    # graph_problem.backtracking_first_solution(0, 0)
-    # print(graph_problem.solutions)
-    # solution_plot = graph_problem.solution[0]
-    # graph.colors = solution_plot
-    # graph.plot_graph()
+    graph = Graph(num_points=30, max_color_num=3)
+    graph_problem = Problem(1, 30, graph.is_save, graph.max_color_num)
+    graph_problem.get_most_used_value_first(0, 0)
+    print(graph_problem.solutions)
+    solution_plot = graph_problem.solution[0]
+    graph.colors = solution_plot
+    graph.plot_graph()
+
+
+    einshtein = Einshtein()
+    einshtein_problem = Problem(5, 5, einshtein.is_save, 5)
+    einshtein_problem.get_most_used_value_first(0, 0)
+    einshtein.solution = einshtein_problem.solution
+    einshtein.print_solution()
+
 
     # graph = Graph(num_points=10, max_color_num=3)
     # graph_problem = Problem(1, 10, graph.is_save, graph.max_color_num)
@@ -158,19 +200,18 @@ if __name__ == "__main__":
     # einshtein1.solution = einshtein_problem1.solution
     # einshtein1.print_solution()
 
-    graph3 = Graph(num_points=10, max_color_num=3)
-    graph4 = deepcopy(graph3)
-    graph_problem = Problem(1, 10, graph3.is_save, d=None, dziedzina=graph3.dziedzina, change_dziedzina=graph3.change_dziedzina)
-    graph_problem.forward_checking_statistic(0, 0)
-    #print(graph_problem.solution)
-    print("Solutions find count: {count}".format(count=len(graph_problem.solutions)))
-    print("Forward checking counter: {count}".format(count=Problem.counter_forward_checking))
-
-    Problem.counter_forward_checking = 0
-
-    graph_problem4 = Problem(1, 10, graph4.is_save, d=graph4.max_color_num, dziedzina=graph4.dziedzina,
-                            change_dziedzina=graph4.change_dziedzina)
-    graph_problem4.backtracking_statistic(0, 0)
-    # print(graph_problem.solution)
-    print("Solutions find count: {count}".format(count=len(graph_problem4.solutions)))
-    print("Backtracking counter: {count}".format(count=Problem.counter_backtracking))
+    # graph3 = Graph(num_points=10, max_color_num=3)
+    # graph4 = deepcopy(graph3)
+    # graph_problem = Problem(1, 10, graph3.is_save, d=None, dziedzina=graph3.dziedzina, change_dziedzina=graph3.change_dziedzina)
+    # graph_problem.forward_checking_statistic(0, 0)
+    # print("Solutions find count: {count}".format(count=len(graph_problem.solutions)))
+    # print("Forward checking counter: {count}".format(count=Problem.counter_forward_checking))
+    #
+    # Problem.counter_forward_checking = 0
+    #
+    # graph_problem4 = Problem(1, 10, graph4.is_save, d=graph4.max_color_num, dziedzina=graph4.dziedzina,
+    #                         change_dziedzina=graph4.change_dziedzina)
+    # graph_problem4.backtracking_statistic(0, 0)
+    # # print(graph_problem.solution)
+    # print("Solutions find count: {count}".format(count=len(graph_problem4.solutions)))
+    # print("Backtracking counter: {count}".format(count=Problem.counter_backtracking))
